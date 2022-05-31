720bccbab95414c39292f6b82957d6addc6a1abb
kgpayne <ken@meltano.com>
2022-05-31 21:34:10 +0100

    move to sphinx (#9)
    * move to sphinx
    
    * first pass at github actions

--- content/pages/about.md
+++
@@ -1,4 +0,0 @@
-title: about
-
-## A SINGLE PAGE
-Pages show up in the menu unless you set `DISPLAY_PAGES_ON_MENU = False` in settings.
--- travis_addS3ID.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-import os
-
-with open(".s3cfg", "w") as myfile:
-    myfile.write("\n")
-    myfile.write("access_key = {0}".format(os.environ['AWS_ACCESS_KEY_ID']))
-    myfile.write("\n")
-    myfile.write("secret_key = {0}".format(os.environ['AWS_SECRET_ACCESS_KEY']))
